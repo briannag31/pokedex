576d28a1953f1d7272090b249e736fbbe6a0d900
Brianna Gaines <briannagaines31@gmail.com>
2022-04-15 10:42:09 -0600

    adds index Route

--- server.js
+++ server.js
@@ -10,7 +10,59 @@ app.use(morgan("tiny"))
 app.use(methodOverride("_method")) 
 app.use("/static", express.static("public"))
 
+app.get("/", (req, res) => {
+    res.send("You are home!");
+  });
+  
+  // Index 
+  app.get("/pokemon/", (req, res) => {
+    //   res.send(pokemon)
+    res.render("index.ejs", { allPokemon: pokemon });
+  });
+  
+  // New Route
+  app.get('/pokemon/new', (req, res) => {
+    res.render('new.ejs');
+  });
+  
+  //Delete
+  app.delete("/pokemon/:id", (req,res)=>{
+    const index = req.params.id
+    pokemon.splice(index, 1)
+    res.redirect("/pokemon")
+  })
 
+  //Update
+ app.put("/pokemon/:id", (req, res) => {
+    pokemon[req.params.id] = req.body
+      res.redirect("/pokemon")
+})
+  
+
+ //Create
+ app.post('/pokemon', (req, res) => {
+    pokemon.push(req.body);
+    console.log(pokemon);
+    res.redirect('/pokemon');
+  });
+
+//edit
+app.get("/pokemon/:id/edit", (req,res)=>{
+     res.render("edit.ejs",{ 
+        poke:pokemon[req.params.id],
+        index:req.params.id
+    })
+})
+
+// Show
+app.get("/pokemon/:id", (req, res) => {
+ res.render("show.ejs", { poke: pokemon[req.params.id] });
+});
+  
+
+  
+  
+  
 
 
 
